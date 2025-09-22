ca88c41ebf1e39607b7119b0905dbd9d6e83bac4
umiversty <baker.coleman@gmail.com>
2025-09-21 20:12:59 -0600

    Fix QuestionPanel filters and tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,11 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vitest/config';
 
-
+export default defineConfig({
+  plugins: [svelte()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    css: true
+  }
 });
