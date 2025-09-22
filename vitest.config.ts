cdceca52ffdacbf329102f9492f354b5bbe6f940
umiversty <baker.coleman@gmail.com>
2025-09-21 19:29:07 -0600

    Configure Vitest for Svelte components

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-
-  }
+  plugins: [svelte()],
+  resolve: {
+    conditions: ['browser'],
+  },
+  test: {
+    environment: 'jsdom',
+  },
 });
